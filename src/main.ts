5760e5793aaeb20cd1b1f193dd0510c752fafd92
Walter Rivero <wrivero1403@gmail.com>
2025-05-11 21:20:50 +0200

    Configure NestJS application to use detailed logging and listen on all interfaces

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
+  });
+  
   
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,7 +17,9 @@ async function bootstrap() {
   
   app.setGlobalPrefix('api/v1');
   
-  await app.listen(3000);
+  // Cambio 1: Puerto 3000 en lugar de 3001
+  // Cambio 2: Especificar '0.0.0.0' para escuchar en todas las interfaces
+  await app.listen(3000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
